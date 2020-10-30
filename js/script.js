c6d71292d688d7710e57f381247901296b7857d7
Dominika Jelonek <jelonekdominika@gmail.com>
2020-10-30 07:42:06 +0100

    Change js - remove class active from links and articles

--- js/script.js
+++ js/script.js
@@ -4,11 +4,24 @@ function titleClickHandler(event) {
     console.log("Link was clicked!");
     console.log(event);
 
-  /* remove class 'active' from all article links  */
+/* remove class 'active' from all article links  */
+    
+    const activeLinks = document.querySelectorAll('.titles a.active');
 
-  /* add class 'active' to the clicked link */
+    for (let activeLink of activeLinks) {
+        activeLink.classList.remove('active');
+    }
 
-  /* remove class 'active' from all articles */
+/* add class 'active' to the clicked link */
+    
+
+/* remove class 'active' from all articles */
+    
+    const activeArticles = document.querySelectorAll('.posts article.active');
+
+    for (let activeArticle of activeArticles) {
+        activeArticle.classList.remove('active');
+    }
 
   /* get 'href' attribute from the clicked link */
 
