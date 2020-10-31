c5f0990ba158e204c324647ad81eb0cfce2f8589
Dominika Jelonek <jelonekdominika@gmail.com>
2020-10-31 11:40:50 +0100

    Add class active to correct article

--- js/script.js
+++ js/script.js
@@ -33,10 +33,13 @@ function titleClickHandler(event) {
 
   /* find the correct article using the selector (value of 'href' attribute) */
 
-  targetArticle = document.querySelector(articleSelector);
+  const targetArticle = document.querySelector(articleSelector);
   console.log(targetArticle);
 
-  /* add class 'active' to the correct article */
+/* add class 'active' to the correct article */
+
+  targetArticle.classList.add("active");
+
 }
 
 const links = document.querySelectorAll(".titles a");
